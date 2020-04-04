9ca2bbf3510344ff9ead761aa6482456904b17d7
AaaNeei <AaaNeei@163.com>
2020-04-04 13:17:22 +0800

    Initial commit

---
+++ common-api/src/main/java/com/st/common/service/CommonService.java
@@ -0,0 +1,11 @@
+package com.st.common.service;
+
+/**
+ * @author neei
+ * @date 2020/4/4 12:21
+ * @desc 游学网
+ */
+
+
+public interface CommonService {
+}
---
+++ common-tool/src/main/java/com/st/common/tool/StException.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.st.common.tool;
+
+/**
+ * 自定义异常
+ *
+ * @author neei
+ *
+ */
+public class StException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+    private String msg;
+    private int code = 500;
+    
+    public StException(String msg) {
+		super(msg);
+		this.msg = msg;
+	}
+	
+	public StException(String msg, Throwable e) {
+		super(msg, e);
+		this.msg = msg;
+	}
+	
+	public StException(String msg, int code) {
+		super(msg);
+		this.msg = msg;
+		this.code = code;
+	}
+	
+	public StException(String msg, int code, Throwable e) {
+		super(msg, e);
+		this.msg = msg;
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
+}
